b762a05c505a815a5614618c6b85628863927be2
nicholasbair <bairinc@gmail.com>
2020-04-20 15:07:46 -0600

    Add explicit types, closes #5.

--- README.md
+++ README.md
@@ -22,15 +22,31 @@ defmodule MyModule do
 
   import Const
 
+  # Implicit type conversion
   def init do
     # fetch/1 will return nil if no value is present
     fetch("AN_ENV")
     |> use_value
   end
 
+  # Implicit type conversion
   def init(arg) do
     # fetch!/1 will raise an error if no value is present
     fetch!("AN_ENV")
+    |> use_value
+  end
+
+  # Explicit type conversion
+  def init do
+    # fetch/2 will return nil if no value is present
+    fetch("AN_ENV", :integer)
+    |> use_value
+  end
+
+  # Explicit type conversion
+  def init(arg) do
+    # fetch!/2 will raise an error if no value is present
+    fetch!("AN_ENV", :integer)
     |> use_value
   end
 
--- lib/const.ex
+++ lib/const.ex
@@ -10,8 +10,16 @@ defmodule Const do
       iex> Const.fetch!("NOT_THERE")
       ** (RuntimeError) Env variable error, no value found for NOT_THERE.
 
+      iex> Const.fetch!("NOT_THERE", :string)
+      ** (RuntimeError) Env variable error, no value found for NOT_THERE.
+
       iex> System.put_env("STRING", "string")
       iex> str = Const.fetch("STRING")
+      iex> is_bitstring(str)
+      true
+
+      iex> System.put_env("STRING", "string")
+      iex> str = Const.fetch("STRING", :string)
       iex> is_bitstring(str)
       true
 
@@ -20,8 +28,18 @@ defmodule Const do
       iex> is_bitstring(acct)
       true
 
+      iex> System.put_env("ACCT", "000-000-000")
+      iex> acct = Const.fetch("ACCT", :string)
+      iex> is_bitstring(acct)
+      true
+
       iex> System.put_env("INT", "123")
       iex> int = Const.fetch("INT")
+      iex> is_integer(int)
+      true
+
+      iex> System.put_env("INT", "123")
+      iex> int = Const.fetch("INT", :integer)
       iex> is_integer(int)
       true
 
@@ -30,8 +48,18 @@ defmodule Const do
       iex> is_integer(int)
       true
 
+      iex> System.put_env("NEG_INT", "-123")
+      iex> int = Const.fetch("NEG_INT", :integer)
+      iex> is_integer(int)
+      true
+
       iex> System.put_env("FLOAT", "1.23")
       iex> flt = Const.fetch("FLOAT")
+      iex> is_float(flt)
+      true
+
+      iex> System.put_env("FLOAT", "1.23")
+      iex> flt = Const.fetch("FLOAT", :float)
       iex> is_float(flt)
       true
 
@@ -40,8 +68,18 @@ defmodule Const do
       iex> is_float(flt)
       true
 
+      iex> System.put_env("NEG_FLOAT", "-1.23")
+      iex> flt = Const.fetch("NEG_FLOAT", :float)
+      iex> is_float(flt)
+      true
+
       iex> System.put_env("BOOL", "true")
       iex> bool = Const.fetch("BOOL")
+      iex> is_boolean(bool)
+      true
+
+      iex> System.put_env("BOOL", "true")
+      iex> bool = Const.fetch("BOOL", :boolean)
       iex> is_boolean(bool)
       true
 
@@ -49,37 +87,78 @@ defmodule Const do
       iex> json = Const.fetch("JSON")
       iex> is_map(json)
       true
+
+      iex> System.put_env("JSON", "{\"testing\": \"things\"}")
+      iex> json = Const.fetch("JSON", :map)
+      iex> is_map(json)
+      true
   """
 
-  @spec fetch!(String.t) :: boolean | float | integer | String.t | map
+  @spec fetch!(String.t()) :: boolean() | float() | integer() | String.t() | map()
   def fetch!(var) do
-    case val = fetch(var) do
+    var
+    |> get!()
+    |> convert()
+  end
+
+  @spec fetch(String.t()) :: nil | boolean() | float() | integer() | String.t() | map()
+  def fetch(var) do
+    var
+    |> get()
+    |> convert()
+  end
+
+  @spec fetch!(String.t(), atom()) :: nil | boolean() | float() | integer() | String.t() | map()
+  def fetch!(var, type) do
+    var
+    |> get!()
+    |> convert(type)
+  end
+
+  @spec fetch(String.t(), atom()) :: nil | boolean() | float() | integer() | String.t() | map()
+  def fetch(var, type) do
+    var
+    |> get()
+    |> convert(type)
+  end
+
+  # -- Private --
+  @spec get(String.t()) :: nil | String.t()
+  defp get(var) do
+    var
+    |> System.get_env
+  end
+
+  @spec get!(String.t()) :: String.t()
+  defp get!(var) do
+    case val = get(var) do
       nil -> raise "Env variable error, no value found for #{var}."
       _ -> val
     end
   end
 
-  @spec fetch(String.t) :: nil | boolean | float | integer | String.t | map
-  def fetch(var) do
-    var
-    |> System.get_env
-    |> convert(var)
+  @spec convert(nil | String.t(), atom()) :: nil | boolean() | float() | integer() | String.t() | map()
+  defp convert(nil, _type), do: nil
+  defp convert(val, :string), do: val
+  defp convert(val, :boolean), do: convert(val)
+  defp convert(val, :map), do: convert_str(val)
+  defp convert(val, type) when type in [:integer, :float, :atom, :existing_atom, :charlist] do
+    apply(String, String.to_atom("to_#{type}"), [val])
   end
 
-  # -- Private --
-  @spec convert(nil | String.t, String.t) :: nil | boolean | float | integer | String.t | map
-  defp convert(val, _var) when val in ["true", "TRUE"], do: true
-  defp convert(val, _var) when val in ["false", "FALSE"], do: false
-  defp convert(nil, _var), do: nil
+  @spec convert(nil | String.t()) :: nil | boolean() | float() | integer() | String.t() | map()
+  defp convert(val) when val in ["true", "TRUE"], do: true
+  defp convert(val) when val in ["false", "FALSE"], do: false
+  defp convert(nil), do: nil
 
-  defp convert(val, _var) do
+  defp convert(val) do
     case String.match?(val, ~r/[a-zA-Z]/) or String.match?(val, ~r/^[\d]+(-)+[\d]/) do
       true -> convert_str(val)
       false -> convert_num(val)
     end
   end
 
-  @spec convert_num(String.t) :: float | integer
+  @spec convert_num(String.t()) :: float() | integer()
   defp convert_num(val) do
     case String.match?(val, ~r/\d\./) do
       true -> String.to_float(val)
@@ -87,7 +166,7 @@ defmodule Const do
     end
   end
 
-  @spec convert_str(String.t) :: map | String.t
+  @spec convert_str(String.t()) :: map() | String.t()
   defp convert_str(val) do
     case is_json?(val) do
       true -> Jason.decode!(val, [keys: :atoms])
@@ -95,7 +174,7 @@ defmodule Const do
     end
   end
 
-  @spec is_json?(String.t) :: boolean
+  @spec is_json?(String.t()) :: boolean()
   defp is_json?(str), do: String.match?(str, ~r/{/)
 
 end
